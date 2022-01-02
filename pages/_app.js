5a8c5bf9dfaaa9d567c58ae8925a978b04c64900
Horacio Damian Brunaccio <85462560+Horuuuu@users.noreply.github.com>
2022-01-01 21:46:38 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import 'tailwindcss/tailwind.css';
 import '../styles.css';
-//import { Provider } from "next-auth/client";
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    //<Provider session={pageProps.session}>
+    
     <Component {...pageProps} />
-    //</Provider>
+    
   );
 }
 
